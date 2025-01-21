7479c601c699227c7d35fd678b1fc68667ffb6b2
pathorn <164243298+pathornn@users.noreply.github.com>
2025-01-21 21:15:07 +0700

    Create SummaryMap.java

---
+++ SummaryMap.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int row = scan.nextInt();
+        int col = scan.nextInt();
+
+        int order = scan.nextInt();
+        int[][] map = new int[row][col];
+        for(int i=0; i<order; i++){
+            int point_row = scan.nextInt();
+            int point_col = scan.nextInt();
+
+            int size_row = scan.nextInt();
+            int size_col = scan.nextInt();
+
+            if(point_row-1 < 0 || point_row-1 > row || point_col-1 < 0 || point_col - 1> col || point_row + size_row - 1 > row || point_col + size_col - 1 > col){
+                continue;                //ถ้าหลุดขอบเขตให้ออก loop ไปทำใหม่
+            }else{
+                boolean avaliable = true;      //เช็คว่าจองพื้นที่ซ้ำหรือป่าว
+                for(int ii=point_row-1; ii<point_row + size_row - 1; ii++){
+                    for(int iii=point_col-1; iii<point_col + size_col - 1; iii++){
+                        if(map[ii][iii] == 1) avaliable = false;   //เจอพื้นที่ซ้ำ (เลข 1)
+                    }
+                }
+    
+                if(avaliable == true){
+                    for(int ii=point_row-1; ii<point_row + size_row - 1; ii++){
+                        for(int iii=point_col-1; iii<point_col + size_col - 1; iii++){
+                            map[ii][iii] = 1;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        for(int i=0; i<row; i++){
+            for(int ii=0; ii<col; ii++){
+                System.out.print(map[i][ii] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
